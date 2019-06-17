af4a263f08c495046098da96edcc27d299e55e98
mahongbin <mahongbin@youxin.com>
2019-06-17 13:34:22 +0800

    添加eureka

--- src/main/java/com/luckysite/LuckysiteApplication.java
+++ src/main/java/com/luckysite/LuckysiteApplication.java
@@ -3,11 +3,14 @@ package com.luckysite;
 import com.github.tobato.fastdfs.FdfsClientConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.EnableMBeanExport;
 import org.springframework.context.annotation.Import;
 import org.springframework.jmx.support.RegistrationPolicy;
 
-
+@EnableFeignClients
+@EnableDiscoveryClient
 @Import(FdfsClientConfig.class)
 // 解决jmx重复注册bean的问题
 @EnableMBeanExport(registration = RegistrationPolicy.IGNORE_EXISTING)
