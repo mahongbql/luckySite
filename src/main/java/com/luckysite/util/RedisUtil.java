19a3a693f16ba9c0f2a04dd602bc8e9c1749c078
mahongbin <mahongbin@youxin.com>
2019-06-18 14:13:37 +0800

    添加eureka

--- src/main/java/com/luckysite/util/RedisUtil.java
+++ src/main/java/com/luckysite/util/RedisUtil.java
@@ -490,7 +490,9 @@ public class RedisUtil {
     public boolean lSet(String key, List<Object> value, long time) {
         try {
             redisTemplate.opsForList().rightPushAll(key, value);
-            if (time > 0) expire(key, time);
+            if (time > 0) {
+                expire(key, time);
+            }
             return true;
         } catch (Exception e) {
             e.printStackTrace();
