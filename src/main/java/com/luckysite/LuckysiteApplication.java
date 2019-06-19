394cc0fedf9ca5347931554c79d32714f9dad22a
mahongbin <mahongbin@youxin.com>
2019-06-19 11:52:28 +0800

    添加eureka

--- src/main/java/com/luckysite/LuckysiteApplication.java
+++ src/main/java/com/luckysite/LuckysiteApplication.java
@@ -3,6 +3,7 @@ package com.luckysite;
 import com.github.tobato.fastdfs.FdfsClientConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.EnableMBeanExport;
 import org.springframework.context.annotation.Import;
 import org.springframework.jmx.support.RegistrationPolicy;
@@ -11,6 +12,7 @@ import org.springframework.jmx.support.RegistrationPolicy;
 // 解决jmx重复注册bean的问题
 @EnableMBeanExport(registration = RegistrationPolicy.IGNORE_EXISTING)
 @SpringBootApplication
+@EnableEurekaClient
 public class LuckysiteApplication {
 
     public static void main(String[] args) {
