dbec7b1dbdd665a08c39a2028b4348242c984baa
mahongbin <mahongbin@youxin.com>
2019-06-20 16:25:35 +0800

    添加日志校验

--- src/main/java/com/luckysite/entity/PostPic.java
+++ src/main/java/com/luckysite/entity/PostPic.java
@@ -1,49 +1,12 @@
 package com.luckysite.entity;
 
+import lombok.Data;
+
+@Data
 public class PostPic {
     private Long id;
     private String uploadName;
     private String url;
     private Long userId;
     private int status;
-
-    public int getStatus() {
-        return this.status;
-    }
-
-    public void setStatus(int status) {
-        this.status = status;
-    }
-
-    public Long getUserId() {
-        return this.userId;
-    }
-
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setUploadName(String uploadName) {
-        this.uploadName = uploadName;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public Long getId() {
-        return this.id;
-    }
-
-    public String getUrl() {
-        return this.url;
-    }
-
-    public String getUploadName() {
-        return this.uploadName;
-    }
 }
