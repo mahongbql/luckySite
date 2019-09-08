3fbaa36bed6a6d9e8576d09ded1a148dd129447d
mahongbin <1318380423@qq.com>
2019-09-08 12:26:38 +0800

    安装lombok 2019-09-08

--- src/main/java/com/luckysite/common/Interceptors/AccessHandlerInterceptor.java
+++ src/main/java/com/luckysite/common/Interceptors/AccessHandlerInterceptor.java
@@ -5,11 +5,7 @@ import com.luckysite.entity.User;
 import com.luckysite.service.UserService;
 import com.luckysite.util.RedisUtil;
 import lombok.extern.slf4j.Slf4j;
-import net.sf.json.JSON;
 import net.sf.json.JSONObject;
-import net.sf.json.JsonConfig;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.method.HandlerMethod;
@@ -18,10 +14,8 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.BufferedReader;
 import java.lang.reflect.Method;
-import java.util.Date;
 
 @Slf4j
 @Component
