2571a71a5967c4029990a30d23221b521ceaa899
mahongbin <1318380423@qq.com>
2019-09-08 12:28:02 +0800

    安装lombok 2019-09-08

--- src/main/java/com/luckysite/common/Interceptors/AccessHandlerInterceptor.java
+++ src/main/java/com/luckysite/common/Interceptors/AccessHandlerInterceptor.java
@@ -67,6 +67,8 @@ public class AccessHandlerInterceptor implements HandlerInterceptor {
             String token = getToken(request);
             String methodName = ((HandlerMethod)obj).getMethod().getName();
 
+            log.info("获取到前端的token -> " + token);
+
             Object userObj = redisUtil.get(token);
             if(null == userObj){
                 log.error("AccessHandlerInterceptor-token失效");
