60ec7faacfdab5790575dc9c0f5465bba119c531
mahongbin <mahongbin@youxin.com>
2019-10-28 10:15:42 +0800

    代码优化 2019-10-28

--- src/main/java/com/luckysite/util/CacheKeyUtil.java
+++ src/main/java/com/luckysite/util/CacheKeyUtil.java
@@ -2,22 +2,34 @@ package com.luckysite.util;
 
 public class CacheKeyUtil {
     /** ------------------- 图片 ------------------- */
-    //图片浏览次数
+    /**
+     * 图片浏览次数
+     */
     public static final String PIC_VIEW_NUMBER = "pic_view_number_";
 
-    //图片收藏次数
+    /**
+     * 图片收藏次数
+     */
     public static final String PIC_COLLECT_NUMBER = "pic_collect_number_";
 
-    //用户收藏记录
+    /**
+     * 用户收藏记录
+     */
     public static final String PIC_COLLECT = "pic_";
 
     /** ------------------- 文章 ------------------- */
-    //图片浏览次数
+    /**
+     * 图片浏览次数
+     */
     public static final String POST_VIEW_NUMBER = "post_view_number_";
 
-    //图片收藏次数
+    /**
+     * 图片收藏次数
+     */
     public static final String POST_COLLECT_NUMBER = "post_collect_number_";
 
-    //用户收藏记录
+    /**
+     * 图片收藏次数
+     */
     public static final String POST_COLLECT = "post_";
 }
