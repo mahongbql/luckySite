944eb79148edc7730976acf25b92beb20d5241c6
mahongbin <mahongbin@youxin.com>
2019-11-04 17:06:25 +0800

    代码优化 2019-11-04

--- src/main/java/com/luckysite/service/impl/UserServiceImpl.java
+++ src/main/java/com/luckysite/service/impl/UserServiceImpl.java
@@ -16,6 +16,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
 import java.util.List;
 
 @Service
@@ -23,7 +24,7 @@ public class UserServiceImpl implements UserService {
 
     private Logger log = LoggerFactory.getLogger(UserServiceImpl.class);
 
-    @Autowired
+    @Resource
     private UserMapper userMapper;
 
     @Override
