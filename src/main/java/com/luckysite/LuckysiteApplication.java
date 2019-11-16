fca0087e1962773caa4944c165a2d08ed910dd63
ma hongb <your_email@youremail.com>
2019-11-16 12:01:21 +0800

    功能展示 2019-11-16 mahongbin

--- src/main/java/com/luckysite/LuckysiteApplication.java
+++ src/main/java/com/luckysite/LuckysiteApplication.java
@@ -1,19 +1,18 @@
 package com.luckysite;
 
 import com.github.tobato.fastdfs.FdfsClientConfig;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableMBeanExport;
 import org.springframework.context.annotation.Import;
 import org.springframework.jmx.support.RegistrationPolicy;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Import(FdfsClientConfig.class)
 // 解决jmx重复注册bean的问题
 @EnableMBeanExport(registration = RegistrationPolicy.IGNORE_EXISTING)
 @SpringBootApplication(scanBasePackages = "com.luckysite")
+@MapperScan(basePackages = "com.luckysite.mapper")
 public class LuckysiteApplication {
 
     public static void main(String[] args) {
