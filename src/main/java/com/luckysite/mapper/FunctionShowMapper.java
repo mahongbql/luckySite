5d817cc5512f944e49ba2f222788e7ca1610f18b
ma hongb <your_email@youremail.com>
2019-11-16 11:50:25 +0800

    功能展示 2019-11-16 mahongbin

--- src/main/java/com/luckysite/mapper/FunctionShowMapper.java
+++ src/main/java/com/luckysite/mapper/FunctionShowMapper.java
@@ -2,7 +2,6 @@ package com.luckysite.mapper;
 
 import com.luckysite.entity.FunctionShow;
 import org.apache.ibatis.annotations.Mapper;
-import tk.mybatis.mapper.common.BaseMapper;
 
 @Mapper
 public interface FunctionShowMapper extends BaseMapper<FunctionShow> {
