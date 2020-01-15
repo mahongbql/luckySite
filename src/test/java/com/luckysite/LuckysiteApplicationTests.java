c3ec740539c1da8d8b2b23b1e94e1f37b757dbe4
mahongbin <mahongbin@youxin.com>
2020-01-15 15:17:13 +0800

    解决bug 2020-01-15

--- src/test/java/com/luckysite/LuckysiteApplicationTests.java
+++ src/test/java/com/luckysite/LuckysiteApplicationTests.java
@@ -1,9 +1,7 @@
 package com.luckysite;
 
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
