adb9d25b8a01a4dc9062583a6b74dacec7e563e2
mahongbin <1318380423@qq.com>
2020-02-20 16:27:25 +0800

    优化上传 2020-02-18

--- src/main/java/com/luckysite/LuckysiteApplication.java
+++ src/main/java/com/luckysite/LuckysiteApplication.java
@@ -3,17 +3,35 @@ package com.luckysite;
 import com.github.tobato.fastdfs.FdfsClientConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableMBeanExport;
 import org.springframework.context.annotation.Import;
 import org.springframework.jmx.support.RegistrationPolicy;
+
+import javax.servlet.MultipartConfigElement;
 
 @Import(FdfsClientConfig.class)
 // 解决jmx重复注册bean的问题
 @EnableMBeanExport(registration = RegistrationPolicy.IGNORE_EXISTING)
 @SpringBootApplication(scanBasePackages = "com.luckysite")
+@Configuration
 public class LuckysiteApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LuckysiteApplication.class, args);
     }
+
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        // 单个数据大小
+        factory.setMaxFileSize("10240KB"); // KB,MB
+        // 总上传数据大小
+        factory.setMaxRequestSize("102400KB");
+        return factory.createMultipartConfig();
+    }
+
+
 }
